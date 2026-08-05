8b4acb165ff7db5bef2ba4edbe95f3845738c91e
maria eduarda summchen <04595523092@senacrs.edu.br>
2026-08-05 10:13:55 -0300

    atualizacoes do projeto
    .

--- portifolio/src/components/About/About.jsx
+++ portifolio/src/components/About/About.jsx
@@ -21,27 +21,21 @@ function About() {
         <div className="about-info">
 
           <div className="card">
-            <span>🎓</span>
-            <h3>Formação</h3>
-            <p>Ensino Médio Técnico em Informática para Internet</p>
+           
+            
           </div>
 
           <div className="card">
-            <span>📍</span>
-            <h3>Localização</h3>
-            <p>Porto Alegre - RS</p>
+          
+           
           </div>
 
           <div className="card">
-            <span>💼</span>
-            <h3>Objetivo</h3>
-            <p>Primeira oportunidade como Desenvolvedora Front-end</p>
+           
           </div>
 
           <div className="card">
-            <span>💻</span>
-            <h3>Stack</h3>
-            <p>React • JavaScript • HTML • CSS • PHP • MySQL</p>
+           
           </div>
 
         </div>
